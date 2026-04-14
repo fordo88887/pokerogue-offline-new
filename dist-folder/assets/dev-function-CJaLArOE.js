32d41c706365382f380ec8c2044dbc1e6d7f11d6
fordo88887 <fordo88887@gmail.com>
2026-04-14 05:34:02 +0000

    added dist

---
+++ dist-folder/assets/dev-function-CJaLArOE.js
@@ -0,0 +1,2 @@
+var t=Object.defineProperty;var n=(c,o)=>t(c,"name",{value:o,configurable:!0});async function e(){}n(e,"customDevFunction");export{e as customDevFunction};
+//# sourceMappingURL=dev-function-CJaLArOE.js.map
